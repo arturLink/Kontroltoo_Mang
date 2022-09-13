611cbbd6afb1cea8c97b25757d305f46dc5a805f
opilane <opilane@HP-CZC6297B8L>
2022-09-13 14:56:06 +0300

    pup

--- Kontroltoo_Mang/Character.cs
+++ Kontroltoo_Mang/Character.cs
@@ -6,15 +6,49 @@ using System.Threading.Tasks;
 
 namespace Kontroltoo_Mang
 {
-    internal class Character : IEntity
+    internal class Character : IEntity, IComparable<Character>
     {
         string nimi;
-        List<Ese> tegelased;
+        List<Ese> esed;
 
-        public Character(string nimi, List<Ese> tegelased)
+        public Character(string nimi, List<Ese> esed)
         {
             this.nimi = nimi;
-            this.tegelased = tegelased;
+            this.esed = esed;
+        }
+
+        public void lisaEse(Ese item)
+        {
+            esed.Add(item);
+        }
+
+        public int PunktideArv()
+        {
+            int sum = 0;
+            foreach (Ese x in esed) 
+            { 
+                sum += x.PunktideArv(); 
+            }
+            return sum;
+        }
+        
+        public string Info()
+        {
+            return $"{this.nimi}. This character has {esed.Count} items and those items give {PunktideArv()} points";
+        }
+
+        public void väljastaEsemed()
+        {
+            foreach (Ese x in esed)
+            {
+                Console.WriteLine("{0}\n", Info());
+            }
+        }
+
+        public int CompareTo(Character? other)
+        {
+            if (other == null) return 1;
+            return this.esed.Count() - other.esed.Count();
         }
 
     }
--- Kontroltoo_Mang/Program.cs
+++ Kontroltoo_Mang/Program.cs
@@ -1,2 +1,2 @@
-﻿// kod artura! i tolko artura! A Artur genius!
-Console.WriteLine("Hello, World!");
+﻿using Kontroltoo_Mang;
+// kod artura! i tolko artura! A Artur genius!
